4c94bc56803f7fe82ee38286b13fb5e996117704
Martijn Kerssing <martijn@kerssing.nl>
2025-05-27 13:29:51 +0200

    MK: logonaam aangepast

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 // public/includes/header.php
 ?>
 <header class="bg-[#00477e] text-white shadow p-4 flex items-center justify-between">
-  <img src="../logo.png" alt="Logo" class="h-12" />
+  <img src="../logo_de_spil.png" alt="Logo" class="h-12" />
   <nav class="space-x-4">
     <a href="/" class="hover:underline">Start</a>
     <a href="/ouderbijdrage" class="hover:underline">Ouderbijdrage</a>
